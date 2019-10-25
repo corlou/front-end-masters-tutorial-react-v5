a79ccf8924a25c10e5a3cba96a58a90958e9056a
Corina Pelichowski <corina@danitt.com>
2019-10-25 12:05:16 +1000

    addition of prettier, lint, and parcel

--- src/app.js
+++ src/app.js
@@ -29,39 +29,45 @@ const App = () => {
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));*/
 
-
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
-
 const App = () => {
-    return React.createElement(
-        "div",
-        { id: "something-important" },
-        [
-            React.createElement("h1",  { key: 'Pet 0' }, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese"},
-                { key: 'Pet 1' }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel"},
-                { key: 'Pet 2' }),
-            React.createElement(Pet, {
-                name: "Doink",
-                animal: "Cat",
-                breed: "Mixed"},
-                { key: 'Pet 3' })
-        ]
-    );
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", { key: "Pet 0" }, "Adopt Me!"),
+    React.createElement(
+      Pet,
+      {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      },
+      { key: "Pet 1" }
+    ),
+    React.createElement(
+      Pet,
+      {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      },
+      { key: "Pet 2" }
+    ),
+    React.createElement(
+      Pet,
+      {
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed"
+      },
+      { key: "Pet 3" }
+    )
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
