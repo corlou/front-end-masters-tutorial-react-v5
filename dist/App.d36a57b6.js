036dc71be61517ff1c2fac8423698820a61db041
Corina Pelichowski <corina@danitt.com>
2019-10-26 14:03:37 +1000

    clean up JS code

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -31805,79 +31805,22 @@ var _Pet = _interopRequireDefault(require("./Pet"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/*const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", { key: 'Luna' }, "Luna"),
-        React.createElement("h2", { key: 'Dog' }, "Dog"),
-        React.createElement("h2", { key: 'Havanese' }, "Havanese")
-    ]);
-};
-
-const App = () => {
-    return React.createElement( "div", { id: "something-important"}, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, { 
-            name: "Luna", 
-            animal: "Dog", 
-            breed: "Havanese"
-            }),
-        React.createElement(Pet, { 
-              name: "Pepper", 
-              animal: "Bird", 
-              breed: "Cockatiel"
-            }),
-        React.createElement(Pet, { 
-              name: "Doink", 
-              animal: "Cat", 
-              breed: "Mixed"
-        })
-    ]);
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));*/
 var App = function App() {
-  /*return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", { key: "Pet 0" }, "Adopt Me!"),
-    React.createElement(
-      Pet,
-      {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      },
-      { key: "Pet 1" }
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      },
-      { key: "Pet 2" }
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed"
-      },
-      { key: "Pet 3" }
-    )
-  ]);*/
   return _react.default.createElement("div", null, _react.default.createElement("h1", null, "Adopt Me!"), _react.default.createElement(_Pet.default, {
     name: "Luna",
     animal: "Dog",
-    breed: "Havanese"
+    breed: "Havanese",
+    key: "Pet1"
   }), _react.default.createElement(_Pet.default, {
     name: "Pepper",
     animal: "Bird",
-    breed: "Cockatiel"
+    breed: "Cockatiel",
+    key: "Pet2"
   }), _react.default.createElement(_Pet.default, {
     name: "Doink",
     animal: "Cat",
-    breed: "Mixed"
+    breed: "Mixed",
+    key: "Pet3"
   }));
 };
 
--- src/app.js
+++ src/app.js
@@ -1,76 +1,14 @@
-/*const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", { key: 'Luna' }, "Luna"),
-        React.createElement("h2", { key: 'Dog' }, "Dog"),
-        React.createElement("h2", { key: 'Havanese' }, "Havanese")
-    ]);
-};
-
-const App = () => {
-    return React.createElement( "div", { id: "something-important"}, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, { 
-            name: "Luna", 
-            animal: "Dog", 
-            breed: "Havanese"
-            }),
-        React.createElement(Pet, { 
-              name: "Pepper", 
-              animal: "Bird", 
-              breed: "Cockatiel"
-            }),
-        React.createElement(Pet, { 
-              name: "Doink", 
-              animal: "Cat", 
-              breed: "Mixed"
-        })
-    ]);
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));*/
-
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  /*return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", { key: "Pet 0" }, "Adopt Me!"),
-    React.createElement(
-      Pet,
-      {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      },
-      { key: "Pet 1" }
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      },
-      { key: "Pet 2" }
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed"
-      },
-      { key: "Pet 3" }
-    )
-  ]);*/
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" key="Pet1" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" key="Pet2" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" key="Pet3" />
     </div>
   );
 };
