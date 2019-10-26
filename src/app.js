e13a6391a2db222ab2e7d727ef0cd3103d06941d
Corina Pelichowski <corina@danitt.com>
2019-10-26 14:18:39 +1000

    setting a default location in Search box

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" key="Pet1" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" key="Pet2" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" key="Pet3" />
+      <SearchParams />
     </div>
   );
 };
