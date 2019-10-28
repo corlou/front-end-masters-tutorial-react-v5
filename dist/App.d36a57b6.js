62b26584284fc5b12ff2b8b26c72073f6454b22d
Corina Pelichowski <corina@danitt.com>
2019-10-28 18:50:36 +1000

    async function

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -33526,6 +33526,10 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; if (obj != null) { var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
@@ -33556,6 +33560,47 @@ var SearchParams = function SearchParams() {
       BreedDropdown = _useDropdown4[1],
       setBreed = _useDropdown4[2];
 
+  var _useState5 = (0, _react.useState)([]),
+      _useState6 = _slicedToArray(_useState5, 2),
+      pets = _useState6[0],
+      setPets = _useState6[1];
+
+  function requestPets() {
+    return _requestPets.apply(this, arguments);
+  }
+
+  function _requestPets() {
+    _requestPets = _asyncToGenerator(
+    /*#__PURE__*/
+    regeneratorRuntime.mark(function _callee() {
+      var _ref3, animals;
+
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.next = 2;
+              return _pet.default.animals({
+                location: location,
+                breed: breed,
+                type: animal
+              });
+
+            case 2:
+              _ref3 = _context.sent;
+              animals = _ref3.animals;
+              setPets(animals || []);
+
+            case 5:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+    return _requestPets.apply(this, arguments);
+  }
+
   (0, _react.useEffect)(function () {
     setBreeds([]);
     setBreed("");
@@ -33571,7 +33616,12 @@ var SearchParams = function SearchParams() {
   }, [animal, setBreed, setBreeds]);
   return _react.default.createElement("div", {
     className: "search-params"
-  }, _react.default.createElement("h1", null, location), _react.default.createElement("form", null, _react.default.createElement("label", {
+  }, _react.default.createElement("h1", null, location), _react.default.createElement("form", {
+    onSubmit: function onSubmit(e) {
+      e.preventDefault();
+      requestPets();
+    }
+  }, _react.default.createElement("label", {
     htmlFor: "location"
   }, "Location", _react.default.createElement("input", {
     id: "location",
@@ -33597,7 +33647,9 @@ var _SearchParams = _interopRequireDefault(require("./SearchParams"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var App = function App() {
-  return _react.default.createElement("div", null, _react.default.createElement("h1", null, "Adopt Me!"), _react.default.createElement(_SearchParams.default, null));
+  return _react.default.createElement(_react.default.StrictMode, null, _react.default.createElement("div", null, _react.default.createElement("h1", {
+    id: "something-important"
+  }, "Adopt Me!"), _react.default.createElement(_SearchParams.default, null)));
 };
 
 (0, _reactDom.render)(_react.default.createElement(App, null), document.getElementById("root"));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,7 +7,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
 
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
   useEffect(() => {
     setBreeds([]);
     setBreed("");
@@ -21,7 +31,12 @@ const SearchParams = () => {
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
